fe64ae6e36e28fb7023e650ffe3a0573c28cd45f
liwencan <13802885134@139.com>
2023-02-04 10:06:16 +0800

    cyclicBarrier循环阑珊在交替打印foobar中的使用

--- javaCode/src/MuThread/交替打印FooBar1115.java
+++ javaCode/src/MuThread/交替打印FooBar1115.java
@@ -1,16 +1,15 @@
 package MuThread;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
 
 /**
  * https://leetcode.cn/problems/print-foobar-alternately/description/
+ *
  * @author leewencan
  * @date 2023/1/28 16:27
  */
 public class 交替打印FooBar1115 {
-    private int n;
+    int n;
 
     public 交替打印FooBar1115(int n) {
         this.n = n;
@@ -43,13 +42,12 @@ public class 交替打印FooBar1115 {
      * 然而，实际上并没有真实的许可证对象供线程使用，Semaphore只是对可用的数量进行管理维护
      * 总结：如果线程要访问一个资源就必须先获得信号量。如果信号量内部计数器大于0，信号量减1，然后允许共享这个资源；否则，如果信号量的计数器等于0，信号量将会把线程置入休眠直至计数器大于0.当信号量使用完时，必须释放
      */
-    static class Foobar1 {
-        private int n;
+    static class Foobar_Semaphore extends 交替打印FooBar1115 {
         Semaphore semaphoreFoo = new Semaphore(1);
         Semaphore semaphoreBar = new Semaphore(0);
 
-        public Foobar1(int n) {
-            this.n = n;
+        public Foobar_Semaphore(int n) {
+            super(n);
         }
 
         public void foo(Runnable printFoo) throws InterruptedException {
@@ -69,22 +67,72 @@ public class 交替打印FooBar1115 {
         }
     }
 
-    public static void main(String a[]){
+    /**
+     * 循环栅栏，适合协调多个线程同步执行操作的场景，所有线程等待完成后一起做某件事情
+     */
+    public static class Foobar2_CyclicBarrier extends 交替打印FooBar1115{
+        private CyclicBarrier cyclicBarrier = new CyclicBarrier(2);//表示有两个线程参与等待
+        //因为要控制先后，所以要用一个标志位表示foo先打印
+        volatile boolean fooExec = true;
+
+        public Foobar2_CyclicBarrier(int n) {
+            super(n);
+        }
+
+
+        public void foo(Runnable printFoo) {
+            for (int i = 0; i < n; i++) {
+                while (!fooExec) {
+
+                }
+                printFoo.run();
+                fooExec = false;
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void bar(Runnable printBar) {
+            for (int i = 0; i < n; i++) {
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                printBar.run();
+                fooExec = true;
+            }
+        }
+    }
+
+
+    public static void main(String a[]) {
+        Runnable printFoo = () -> {
+            System.out.print("foo");
+        };
+        Runnable printBar = () -> {
+            System.out.print("bar");
+        };
         ExecutorService executorService = Executors.newCachedThreadPool();
-        Foobar1 foobar1 = new Foobar1(10);
-        executorService.submit(new Thread(()->{
+//        交替打印FooBar1115 交替打印FooBar1115 = new Foobar_Semaphore(10);
+        交替打印FooBar1115 交替打印FooBar1115 = new Foobar2_CyclicBarrier(10);
+        executorService.submit(new Thread(() -> {
             try {
-                foobar1.foo(new Thread(()->System.out.print("foo")));
+                交替打印FooBar1115.foo(new Thread(() -> System.out.print("foo")));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }));
-        executorService.submit(new Thread(()->{
+        executorService.submit(new Thread(() -> {
             try {
-                foobar1.bar(new Thread(()->System.out.println("bar")));
+                交替打印FooBar1115.bar(new Thread(() -> System.out.println("bar")));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }));
+        executorService.shutdown();
     }
 }
